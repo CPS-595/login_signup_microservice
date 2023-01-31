10f0271f6a0a468c12fca5b5914eeb935dd7a7bd
Omkar Karnik <omkarkarnik@Omkars-MacBook-Pro.local>
2023-01-31 01:37:12 -0500

    post requests for login and signup

--- index.js
+++ index.js
@@ -7,63 +7,101 @@ app.use(cors())
 app.use(express.static('static'))
 app.use(express.urlencoded({extended:false}))
 const MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json();
+// var bodyParser = require('body-parser')
+// var jsonParser = bodyParser.json();
 
+// MongoDB url which allows connection to be established with our application
 const mongourl = "mongodb+srv://karniko1:karniko1@cps-595-p1.n8rfp91.mongodb.net/CPS595-Project?retryWrites=true&w=majority";
-
-
 
 const dbClient = new MongoClient(mongourl,
                     {useNewUrlParser: true, useUnifiedTopology: true});
+
 dbClient.connect(err => {
     if (err) throw err;
-    console.log("Connected to the MongoDB cluster ");
+    console.log("Connected to the MongoDB cluster!");
 });
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// Default route
 app.get("/",(req,res) =>{
     res.send("LoginSignup microservice by Omkar Sunil Karnik")
 })
 
-app.get("/Login/:email/:password",(req,res)=> {
-    var checkData = checklogindata(req.params)
-    if (checkData.status =="passed"){
-        console.log(req.params.email,req.params.password) 
-        const db = dbClient.db();
-        db.collection("users").findOne({email: req.params.email, password: req.params.password}, (err, userdata) => {
-        console.log(userdata,"Userdata")
-        if (err){
-            return res.send({message:"Error in Microservice !",status: "authentication-failed"})
-        }
-        if  (userdata){
-            console.log(userdata);
-            return res.send({userdata:userdata, status : "authenticated"});
-            console.log(userdata)
-        }
-        else {
-            return res.send({message:"User Not Found !",status: "authentication-failed"})
-        }
-    });
-    }
-    else{
-        return res.send({message:checkData.message,status:"authentication-failed"})
 
-    }
+
+// app.post("/Login/:username/:password", jsonParser, (req,res)=> {
+//     var checkData = checklogindata(req.params)
+//     if (checkData.status =="passed"){
+//         console.log(req.params.username,req.params.password) 
+//         const db = dbClient.db();
+//         db.collection("Users").findOne({username: req.params.username, password: req.params.password}, (err, userdata) => {
+//         console.log(userdata,"Userdata")
+//         if (err){
+//             return res.send({message:"Error in Microservice !",status: "authentication-failed"})
+//         }
+//         if  (userdata){
+//             console.log(userdata);
+//             return res.send({userdata:userdata, status : "authenticated"});
+//             console.log(userdata)
+//         }
+//         else {
+//             return res.send({message:"User Not Found !",status: "authentication-failed"})
+//         }
+//     });
+//     }
+//     else{
+//         return res.send({message:checkData.message,status:"authentication-failed"})
+
+//     }
+    
+// })
+
+// Login route to which makes post request and checks the data with the database collection
+    
+app.post("/login", (req,res)=>{
+    console.log("inside login",req.body)
+    var checkData = checklogindata(req.body)
+    if (checkData.status =="passed"){
+        console.log('in login', req.body)
+        const db = dbClient.db();
+        db.collection("users").findOne({email: req.body.email, password: req.body.password}, (err, userdata) => {
+            console.log(userdata,"Userdata")
+            if (err){
+                return res.send({message:"Error in Microservice !",status: "authentication-failed"})
+            }
+            if  (userdata){
+                console.log(userdata);
+                return res.send({userdata:userdata, status : "authenticated"});
+                console.log(userdata)
+            }
+            else {
+                return res.send({message:"User Not Found !",status: "authentication-failed"})
+            }
+        });
+        }
+        else{
+            return res.send({message:checkData.message,status:"authentication-failed"})
+    
+        }
     
 })
 
-app.post("/SignUp", jsonParser, (req,res)=>{
+app.post("/signup", (req,res)=>{
     var checkData = checkuserdata(req.body)
     if (checkData.status =="passed"){
         console.log('in signup', req.body)
         const db = dbClient.db();
-        db.collection("Users").findOne({ $or: [{email: req.body.email} , {username: req.body.username}]}, (err, userdata) => {
+        db.collection("users").findOne({ $or: [{email: req.body.email} , {number: req.body.number}]}, (err, userdata) => {
                 if  (userdata){
     
                     return res.send({message:"User Already Exist",status:"registeration-failed"});
                 }
                 else {
-                    db.collection("Users").insertOne({fullname: req.body.fullname ,email: req.body.email, username: req.body.username, password: req.body.password}, (err, userdata) => {
+                    db.collection("users").insertOne({email: req.body.email, password: req.body.password, number: req.body.number}, (err, userdata) => {
                         console.log(userdata,"Userdata")
                             if  (err){
                                 
@@ -81,41 +119,43 @@ app.post("/SignUp", jsonParser, (req,res)=>{
     }
     
 })
+
 function checklogindata(user){
-    // if (!user.username){
-    //     return {status:"failed",message:"no username !"}
-    // }
+    if (!user.email){
+        return {status:"failed",message:"no username !"}
+    }
+    if (ValidateEmail(user.email) != true){
+        return {status:"failed",message:"invalid email !"}
+    }
     if (!user.password){
         return {status:"failed",message:"no password !"}
     }
-    // if (user.username.length < 4 || user.username.length > 20){
-    //     return {status:"failed",message:"username should be between 4 and 20 !"}
-    // } 
-    
+
     if (user.password.length < 6 || user.password.length > 20){
         return {status:"failed",message:"password should be between 6 and 20  !"}
     }
     return {status: 'passed'}
 }   
+
 function checkuserdata(user){
-    // if (!user.username){
-    //     return {status:"failed",message:"no username !"}
-    // }
+    
     if (!user.email){
         return {status:"failed",message:"no email !"}
     }
-    // if (!user.fullname){
-    //     return {status:"failed",message:"no name !"}
-    // }
     if (!user.password){
         return {status:"failed",message:"no password !"}
     }
-    // if (user.username.length < 4 || user.username.length > 20){
-    //     return {status:"failed",message:"username should be between 4 and 20 !"}
-    // }
-    // if (user.password.length < 6 || user.password.length > 20){
-    //     return {status:"failed",message:"password should be between 6 and 20 !"}
-    // }
+    if (!user.number){
+        return {status:"failed",message:"no number !"}
+    }
+    if (isValid_Mobile_Number(user.number) != true){
+        return {status:"failed",message:"invalid email !"}
+    }
+
+   
+    if (user.password.length < 6 || user.password.length > 20){
+        return {status:"failed",message:"password should be between 6 and 20 !"}
+    }
     if (ValidateEmail(user.email) != true){
         return {status:"failed",message:"invalid email !"}
     }
@@ -126,4 +166,19 @@ function ValidateEmail(email) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
         return (true)
     }        return (false)
+}
+
+function isValid_Mobile_Number(mobile_number) {
+    // Regex to check valid
+    // mobile_number 
+    let regex = new RegExp(/[6-9][0-9]{9}/);
+ 
+    // if mobile_number
+    // is empty return false
+    if (mobile_number == null) {
+        return "false";
+    }
+    
+    return regex.test(mobile_number);
+    
 }
