a211920980422780782028efbe37ade802e4cccf
Omkar Karnik <omkarkarnik@Omkars-MacBook-Pro.local>
2023-04-25 04:38:55 -0400

    share

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,14 @@
+const e = require('express');
+const EncryptedPassword = require('../model/EncryptedPassword');
 const User = require('../model/User');
+
+function validateResource(data){
+    if (!data.userId) return { status: 'Failed', message: 'Name is required' };      
+    if (!data.credentialId) return { status: 'Failed', message: 'Username is required' };
+    if (!data.password) return { status: 'Failed', message: 'password is required' };
+    return { status:'success'}
+
+}
 
 const getAllUsers = async(req, res) => {
 
@@ -25,12 +35,35 @@ const storePublicKey = (req, res) => {
     var query = { _id : req.user._id};
     User.findOneAndUpdate(query, {publicKey: publicKey}, {upsert: true}, function(err, result) {
         console.log("result", result)
-        if (err) return res.status(400).json({status: "failed", error: err});
-        return res.status(200).json({status:"Sucess", payload: "Saved Successfully"});
+        if (err) return res.status(400).json({status: "Failed", error: err});
+        return res.status(200).json({status:"Success", payload: "Saved Successfully"});
     });
-
 }
+
+const storeCredential = async (req,res) => {
+    console.log("in storecredential", req.body);
+
+    const checkResource = validateResource(req.body);
+    if (checkResource.status === "success") {
+
+        EncryptedPassword.create({
+            "userId": req.body.userId,
+            "credentialId": req.body.credentialId,
+            "password": req.body.password,
+        }, function(err, result) {
+            console.log("result", result)
+            if (err) return res.status(400).json({status: "Failed", error: err});
+            return res.status(201).json({ status: 'Success', payload: result });
+        })
+    }
+    else {
+        return res.status(400).json(checkResource);
+    }
+    
+}
+
 module.exports = {
     getAllUsers,
-    storePublicKey
+    storePublicKey,
+    storeCredential,
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 router.get('/',usersController.getAllUsers)
-
+router.post('/share',usersController.storeCredential)
 router.post('/', usersController.storePublicKey)
 module.exports = router;
