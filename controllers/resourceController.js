06ef3a64d47b7fc13d1e8f1f197c81a5868c74dc
Anisha Chhatwani <chhatwania1@udayton.edu>
2023-05-03 03:57:44 -0400

    small change

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -95,13 +95,20 @@ function validateResource(data){
 }
 
 function validateUrl(string){
-    let url;
-    try {
-        url = new URL(string);
-    } catch (_) {
-        return false;
-    }
-    return url.protocol === "http:" || url.protocol === "https:";
+    // let url;
+    // try {
+    //     url = new URL(string);
+    // } catch (_) {
+    //     return false;
+    // }
+    // return url.protocol === "http:" || url.protocol === "https:";
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+      '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+        return !!urlPattern.test(string);
       
 }
 
